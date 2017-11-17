d804e24787e7f4ae3eda0c3416a59fb42a43a58a
ChanderG <chanderg@chandergovind.org>
2017-11-17 12:32:36 +0530

    add tab switching functionality
    use dmenu to switch between open tabs
    other addons with same keybindings may hide our command

--- fim-frontend/background.js
+++ fim-frontend/background.js
@@ -8,6 +8,13 @@ Listen for messages from the app.
 */
 port.onMessage.addListener((response) => {
   console.log("Received: " + response);
+
+  if(response.indexOf("switch-tab") === 0){
+    // parse out the number and switch
+    browser.tabs.update(Number(response.split(' ')[1]), {
+      active: true
+    });
+  }
 });
 
 /*
@@ -16,4 +23,36 @@ On a click on the browser action, send the app a message.
 browser.browserAction.onClicked.addListener(() => {
   console.log("Sending:  ping");
   port.postMessage("ping");
+});
+
+/*
+ * Switch Tab function.
+ */
+const switchTab = () => {
+  browser.tabs.query({currentWindow: true}).then((tabs) => {
+    let msgString = "switch-tab\n";
+    for(let tab of tabs){
+      if (!tab.active) {
+	msgString += tab.id;
+	msgString += " | "
+	msgString += tab.title;
+	msgString += " | "
+	msgString += tab.url;
+	msgString += "\n"
+      }
+    }
+    port.postMessage(msgString);
+  });
+}
+
+/*
+ * Key bindings
+ */
+browser.commands.onCommand.addListener((command) => {
+  console.log("onCommand event received for message: ", command);
+
+  switch(command) {
+    case 'switch-tab':
+      switchTab()
+  }
 });
